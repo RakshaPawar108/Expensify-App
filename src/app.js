1ac9f4518a0bd66d48775dd87bd05ab43cc0916e
Raksha <rakshapawar10.sae@gmail.com>
2020-09-19 19:53:55 +0530

    Add Expensify styles final

--- src/app.js
+++ src/app.js
@@ -10,6 +10,7 @@ import "react-dates/lib/css/_datepicker.css";
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 import { firebase } from "./firebase/firebase";
+import LoadingPage from "./components/LoadingPage";
 
 const store = configureStore();
 
@@ -27,7 +28,7 @@ const renderApp = () => {
   }
 };
 
-ReactDOM.render(<p>Loading...</p>, document.getElementById("app"));
+ReactDOM.render(<LoadingPage />, document.getElementById("app"));
 
 firebase.auth().onAuthStateChanged((user) => {
   if (user) {
